5a5e25abd35b162f40b5c8e5b55f3b04cc1f72f3
Tiago Campêlo <tiago.campelo@liferay.com>
2025-07-04 13:11:08 -0300

    LPD-59461 Move metrics to performance tab

--- modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/main/components/info_panel/tab_content/PerformanceTabContent.tsx
+++ modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/main/components/info_panel/tab_content/PerformanceTabContent.tsx
@@ -3,12 +3,76 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 import {Metrics} from '../../Metrics';
+import {
+	AssetTypeInfoPanelContext,
+	IAssetTypeInfoPanelContext,
+} from '../context';
+
+export type Metric = {
+	comparison: number;
+	title: string;
+	total: number;
+};
+
+const defaultSelectedMetric = 'Impressions';
+
+const metricsMock: Metric[] = [
+	{
+		comparison: 0,
+		title: 'Impressions',
+		total: 11,
+	},
+	{
+		comparison: -12.3,
+		title: 'Views',
+		total: 25321,
+	},
+	{
+		comparison: 32.1,
+		title: 'Downloads',
+		total: 220153310,
+	},
+];
+
+async function fetchComponentData(fileId: number): Promise<Metric[]> {
+	fileId;
+
+	return metricsMock;
+}
 
 const PerformanceTabContent = () => {
-	return <Metrics />;
+	const [metrics, setMetrics] = useState<Metric[]>([]);
+	const [selectedMetric, setSelectedMetric] = useState<string>(
+		defaultSelectedMetric
+	);
+
+	const fileContext = React.useContext<IAssetTypeInfoPanelContext>(
+		AssetTypeInfoPanelContext
+	);
+
+	useEffect(() => {
+		const fetchData = async () => {
+			const data = await fetchComponentData(fileContext.id || 0);
+			setMetrics(data);
+		};
+
+		fetchData();
+	});
+
+	return (
+		<div>
+			{!!metrics.length && (
+				<Metrics
+					metrics={metrics}
+					selectedMetric={selectedMetric}
+					setSelectedMetric={setSelectedMetric}
+				/>
+			)}
+		</div>
+	);
 };
 
 export default PerformanceTabContent;
