b8b5b7843ecafab4e8728236a388160e4c02ba50
Tiago Campêlo <tiago.campelo@liferay.com>
2026-04-22 21:40:20 -0300

    LPD-84340 Create shared MetricCard

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/contacts/components/account/TotalAccounts.tsx
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/contacts/components/account/TotalAccounts.tsx
@@ -1,110 +1,17 @@
 import * as API from 'shared/api';
-import Card from 'shared/components/Card';
-import classNames from 'classnames';
-import ClayIcon from '@clayui/icon';
-import Loading from 'shared/components/Loading';
+import MetricCard from 'shared/components/MetricCard';
 import React from 'react';
-import {getIcon, getStatsColor} from 'shared/util/metrics';
-import {isNil} from 'lodash/fp';
 import {Metric} from '../../pages/account/utils/types';
 import {sub} from 'shared/util/lang';
-import {Text} from '@clayui/core';
-import {toRounded} from 'shared/util/numbers';
-import {TrendClassification} from 'segment/types';
 import {useRequest} from 'shared/hooks/useRequest';
 
-interface IAccountCardProps {
-	className?: string;
-	description: string;
-	loading?: boolean;
-	metrics: Metric;
-	title: string;
-}
-
-const AccountCard: React.FC<IAccountCardProps> = ({
-	className,
-	description,
-	loading = false,
-	metrics,
-	title
-}) => {
-	if (loading) {
-		return (
-			<Card className={classNames(className, 'flex-fill p-3 w-100')}>
-				<Card.Body>
-					<Loading />
-				</Card.Body>
-			</Card>
-		);
-	}
-
-	return (
-		<Card className={classNames(className, 'flex-fill p-3 w-100')}>
-			<Card.Title>
-				<div className='text-uppercase text-weight-semi-bold'>
-					<Text>{title}</Text>
-				</div>
-			</Card.Title>
-			<Card.Body noPadding>
-				<div className='mt-1'>
-					<Text color='secondary' size={3}>
-						{description}
-					</Text>
-				</div>
-
-				<span className='mt-3 text-lowercase text-weight-semi-bold'>
-					<Text size={7}>
-						{sub(
-							metrics?.value === 1
-								? Liferay.Language.get('x-account')
-								: Liferay.Language.get('x-accounts'),
-							[metrics?.value ?? 0]
-						)}
-					</Text>
-				</span>
-
-				<span className='text-secondary'>
-					{!isNil(metrics?.trend?.trendClassification) &&
-						metrics?.trend?.trendClassification !==
-							TrendClassification.Neutral && (
-							<ClayIcon
-								style={{
-									color: getStatsColor(
-										metrics?.trend?.trendClassification
-									)
-								}}
-								symbol={
-									getIcon(metrics?.trend?.percentage ?? 0) ??
-									''
-								}
-							/>
-						)}
-					{sub(
-						Liferay.Language.get('x-vs-previous-90-days'),
-						[
-							<span
-								className='mr-1'
-								key='percentage'
-								style={{
-									color:
-										getStatsColor(
-											metrics?.trend?.trendClassification
-										) || TrendClassification.Neutral
-								}}
-							>
-								{`${toRounded(
-									Math.abs(metrics?.trend?.percentage ?? 0),
-									2
-								)}%`}
-							</span>
-						],
-						false
-					)}
-				</span>
-			</Card.Body>
-		</Card>
+const renderAccountValue = (metric?: Metric) =>
+	sub(
+		metric?.value === 1
+			? Liferay.Language.get('x-account')
+			: Liferay.Language.get('x-accounts'),
+		[metric?.value ?? 0]
 	);
-};
 
 const TotalAccounts = ({groupId}: {groupId: string}) => {
 	const {data, loading} = useRequest({
@@ -118,36 +25,53 @@ const TotalAccounts = ({groupId}: {groupId: string}) => {
 
 	const {activeCount, newCount, totalCount} = data || {};
 
+	const renderTrendLabel = (percentageNode: React.ReactNode) =>
+		sub(
+			Liferay.Language.get('x-vs-previous-90-days'),
+			[percentageNode],
+			false
+		);
+
+	const cardContainerClassName = 'col-12 col-lg-4 d-flex';
+
 	return (
-		<div className='d-flex w-100'>
-			<AccountCard
-				className='mr-4'
-				description={Liferay.Language.get(
-					'displays-all-accounts-included-in-this-property'
-				)}
-				loading={loading}
-				metrics={totalCount || {}}
-				title={Liferay.Language.get('total-accounts')}
-			/>
-
-			<AccountCard
-				className='mr-4'
-				description={Liferay.Language.get(
-					'displays-all-new-accounts-included-in-this-property'
-				)}
-				loading={loading}
-				metrics={newCount || {}}
-				title={Liferay.Language.get('new-accounts')}
-			/>
-
-			<AccountCard
-				description={Liferay.Language.get(
-					'displays-all-active-accounts-included-in-this-property'
-				)}
-				loading={loading}
-				metrics={activeCount || {}}
-				title={Liferay.Language.get('active-accounts')}
-			/>
+		<div className='row g-4'>
+			<div className={cardContainerClassName}>
+				<MetricCard
+					description={Liferay.Language.get(
+						'displays-all-accounts-included-in-this-property'
+					)}
+					loading={loading}
+					renderTrendLabel={renderTrendLabel}
+					title={Liferay.Language.get('total-accounts')}
+					trend={totalCount?.trend}
+					value={renderAccountValue(totalCount)}
+				/>
+			</div>
+			<div className={cardContainerClassName}>
+				<MetricCard
+					description={Liferay.Language.get(
+						'displays-all-new-accounts-included-in-this-property'
+					)}
+					loading={loading}
+					renderTrendLabel={renderTrendLabel}
+					title={Liferay.Language.get('new-accounts')}
+					trend={newCount?.trend}
+					value={renderAccountValue(newCount)}
+				/>
+			</div>
+			<div className={cardContainerClassName}>
+				<MetricCard
+					description={Liferay.Language.get(
+						'displays-all-active-accounts-included-in-this-property'
+					)}
+					loading={loading}
+					renderTrendLabel={renderTrendLabel}
+					title={Liferay.Language.get('active-accounts')}
+					trend={activeCount?.trend}
+					value={renderAccountValue(activeCount)}
+				/>
+			</div>
 		</div>
 	);
 };
